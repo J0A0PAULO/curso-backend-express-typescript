fed3713f210dbf2d4b2a03000cbb879e432e9df8
João <moreirajoaopaulo951@gmail.com>
2026-04-10 13:23:24 -0300

    rotas criadas

--- ecommerce-api/src/index.ts
+++ ecommerce-api/src/index.ts
@@ -1,50 +1,9 @@
-import express, { Request, Response } from "express";
-
+import express from "express";
+import { routes } from "./routers/index";
 const app = express();
-app.use(express.json());
 
-let id = 1;
-let usuarios : {id : number, nome : string, email : string}[] = [];
 
-app.get('/', (req : Request, res : Response) => {
-   res.send("Endpoind de usuários");
-});
-
-app.get('/users', (req : Request, res : Response) => {
-  res.send(usuarios);
-});
-
-app.post('/users', (req: Request, res : Response) => {
-  let user = req.body;
-  user.id = id++;
-  usuarios.push(user);
-  res.send({
-    message : 'Usuario criado com sucesso'
-  })
-}); 
-
-app.get('/users/:id', (req : Request, res : Response ) =>{ 
-  let userId = Number(req.params.id);
-  let user = usuarios.findIndex(user => user.id === userId); 
-  res.send(user);
-});
-
-app.put('/users/:id', (req : Request, res : Response ) =>{ 
- let userId = Number(req.params.id);
-  let indice = usuarios.findIndex(user => user.id === userId);
-  if(indice === -1) return res.send('usario nao encontrado');
-  usuarios[indice].email = req.body.nome; 
-  usuarios[indice].email = req.body.email; 
-  res.send('USUARIO ALTERADO COM SUCESSO');
-});
-
-app.delete('/user/:id', (req : Request, res : Response) => {
-  let userId = Number(req.params.id);
-  let indice = usuarios.findIndex(indice => indice.id === userId);
-  if(indice === -1) return res.send('usario nao encontrado');
-  usuarios.splice(indice,1);
-  res.send('USUARIO DELETADO COM SUCESSO')
-});
+routes(app);
 
 app.listen('3000', () => {
   console.log('servidor ativo na porta 3000');
